307044ca31bbf32fe3743806252e6d1c40c8a520
diuliano <diuliano0@gmail.com>
2017-09-26 22:48:15 -0300

    fix

--- config/cors.php
+++ config/cors.php
@@ -12,13 +12,14 @@ return [
      */
     'supportsCredentials' => true,
     'allowedOrigins' => [
-        'http://*.qimob.com.br',
+        '*'
+        /*'http://*.qimob.com.br',
         'http://localhost:9000',
         '*:8000',
         '*:8100',
         '*:4200',
         'chrome-extension://aicmkgpgakddgnaphhhpliifpcfhicfo',
-        'file://'//TODO liberação para consumo do ionic resolver esse origin pois resultaria em falha de segurança.
+        'file://'*///TODO liberação para consumo do ionic resolver esse origin pois resultaria em falha de segurança.
     ],
     'allowedHeaders' => ['*'], //TODO altera os headers liberados para aumentar a segurança
     'allowedMethods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
