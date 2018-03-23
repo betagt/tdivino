162045f07c6e038ce7184b5f947d2b407cec985b
diuliano <diuliano0@gmail.com>
2018-03-22 21:40:49 -0300

    update

--- Modules/Core/Models/User.php
+++ Modules/Core/Models/User.php
@@ -78,7 +78,7 @@ class User extends Authenticatable implements Transformable
             //TODO criar eventos para para esse tipo de ação.
             if(!is_null($query->id) && in_array(User::FORNECEDOR, $query->getRoles())){
                 $validado = app(TipoDocumentoRepository::class)->validadeUser($query->id);
-                $query->status = (!$validado['habilitado'])?User::BLOQUEADO:User::ATIVO;
+                $query->status = ((!$validado['habilitado'] && is_null($query->veiculoAtivo)) || ($validado['habilitado'] && is_null($query->veiculoAtivo)))?User::BLOQUEADO:User::ATIVO;
                 return $query;
             }
         };
