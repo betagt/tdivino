9f37c90baeb4382288473de01a5fea55492841e0
diuliano <diuliano0@gmail.com>
2018-05-17 20:51:07 -0300

    fix

--- Modules/Core/Http/Controllers/Api/Admin/UserController.php
+++ Modules/Core/Http/Controllers/Api/Admin/UserController.php
@@ -761,6 +761,7 @@ class UserController extends BaseController
                 $user->pessoa->update($data['pessoa']);
             }
             if(isset($data['perfil'])){
+                $user->revokeAllRoles();
                 $user->assignRole($data['perfil']);
             }
             if (isset($data['endereco']) && count($data['endereco']) > 0) {
@@ -819,6 +820,7 @@ class UserController extends BaseController
     public function storeCliente(Request $request)
     {
         $data = $request->all();
+        $data = array_remove_null($data);
         if(isset($data['pessoa']['data_nascimento']))
             $data['pessoa']['data_nascimento'] = implode('-', array_reverse(explode('/', $data['pessoa']['data_nascimento'])));
 
@@ -845,7 +847,7 @@ class UserController extends BaseController
             if(isset($data['perfil'])){
                 $user->assignRole($data['perfil']);
             }
-            if (isset($data['endereco'])) {
+            if (isset($data['endereco']) && count($data['endereco']) > 0) {
                 $user->endereco()->save(Endereco::create($data['endereco']));
             }
             if (isset($data['telefone'])) {
@@ -868,7 +870,7 @@ class UserController extends BaseController
             return self::responseError(self::HTTP_CODE_NOT_FOUND, trans('errors.undefined', ['status_code' => $e->getCode(), 'line' => $e->getLine()]));
         } catch (\Exception $e) {
             \DB::rollBack();
-            return self::responseError(self::HTTP_CODE_BAD_REQUEST, trans('errors.undefined', ['status_code' => $e->getCode(), 'line' => $e->getLine()]));
+            return self::responseError(self::HTTP_CODE_BAD_REQUEST, $e->getMessage());
         }
     }
 }
